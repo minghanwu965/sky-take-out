621b6d9cb3001646383559dedb8bb9bc9f4d58d7
minghanwu965 <mingnhanwu965@gmail.com>
2025-11-01 00:37:25 +0800

    启用禁用员工账号代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -18,13 +18,25 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
-//插入员工数据
+    /**
+     * 插入员工数据
+     * @param employee
+     */
     @Insert("insert into employee(name,username,password,phone,sex,id_number,create_time,update_time,create_user,update_user,status)" +
             "values" +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
 
-//分页查询，动态sql
-
+    /**
+     * 分页查询，动态sql
+     * @param employeePageQueryDTO
+     * @return
+     */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 根据id动态地修改属性
+     * @param employee
+     */
+     void update(Employee employee);
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -100,4 +100,24 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new  PageResult(total,records);
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+
+    public void startOrStop(Integer status, Long id) {
+        //创建实体对象
+//        Employee employee=new Employee();
+//        employee.setStatus(status);
+//        employee.setId(id);
+        Employee employee=Employee.builder()
+                        .status(status)
+                                .id(id)
+                                        .build();
+
+        employeeMapper.update(employee);
+    }
+
+
 }
